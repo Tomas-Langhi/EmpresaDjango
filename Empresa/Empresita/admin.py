d3a099d2377121f827a14320b44219980db5810f
Tomas Langhi <tomas-langhi>
2020-06-25 16:16:52 -0300

    Se registraron los modelos en el admin

--- Empresa/Empresita/admin.py
+++ Empresa/Empresita/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from Empresita.models import *
+
+
+
+
+
+
+
+
 
 # Register your models here.
+admin.site.register(Cliente,)
+admin.site.register(Proveedor,)
+admin.site.register(Producto,)
+admin.site.register(Categoria,)
+admin.site.register(Venta,)
+
